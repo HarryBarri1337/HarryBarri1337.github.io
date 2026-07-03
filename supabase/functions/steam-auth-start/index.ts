13532eda0872eb9add1b97e3862db8ae5c9e0288
PLBETS <harrygotesson2@gmail.com>
2026-07-03 23:05:47 +0200

    Update index.ts

--- supabase/functions/steam-auth-start/index.ts
+++ supabase/functions/steam-auth-start/index.ts
@@ -83,7 +83,7 @@ serve(async (req) => {
       "openid.ns": "http://specs.openid.net/auth/2.0",
       "openid.mode": "checkid_setup",
       "openid.return_to": `${callbackUrl}?state=${state}`,
-      "openid.realm": siteUrl,
+      "openid.realm": supabaseUrl,
       "openid.identity": "http://specs.openid.net/auth/2.0/identifier_select",
       "openid.claimed_id": "http://specs.openid.net/auth/2.0/identifier_select",
     });
